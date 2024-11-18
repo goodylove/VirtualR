3ffdbc677714a3e10623785fa338da6d6159bca9
goodylove <goodyc474@gmail.com>
2024-11-18 08:20:00 +0100

    minor fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -49,7 +49,7 @@ function NavBar() {
 
             </div>
             {openMobileMenu &&
-             <div className="fixed  right-0 z-50 bg-neutral-900 w-full p-12 flex flex-col justify-center lg:hidden items-center">
+             <div className="fixed  right-0  z-50 bg-neutral-900 w-full p-12 flex flex-col justify-center lg:hidden items-center">
                 <ul className="">
                   {navItems.map((nav)=>(
                     <li key={nav.label} className="py-4">
@@ -60,9 +60,9 @@ function NavBar() {
                 </ul>
                 
 
-                  <div className=" justify-center  mt-2 flex space-x-12 items-center">
-                  <a href="#" className="py-2 px-3 rounded-md border">Sign in</a>
-                  <a href="" className="bg-gradient-to-r from-orange-500 to-orange-800 px-3 py-2 rounded-md">
+                  <div className=" justify-center  flex-col  mt-2 flex space-y-6 items-center">
+                  <a href="#" className="py-2 px-3 rounded-md border text-xs md:text-base">Sign in</a>
+                  <a href="" className="bg-gradient-to-r from-orange-500 text-xs md:text-base to-orange-800 px-3 py-2 rounded-md">
                     Create  an account
                     
                   </a>
